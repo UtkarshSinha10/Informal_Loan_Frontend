58e5c200cde4698e381696c5a760980274d7f145
UtkarshSinha10 <utkarshsinha1010@gmail.com>
2021-09-18 23:31:59 +0530

    react package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  constructor() {
+    super();
+    this.page = "landingPage";
+  }
+  render() {
+    let a;
+    switch (this.page) {
+      case "signIn":
+        a=<p>
+          <div>{this.page}</div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        break;
+      case "signUp":
+        a=<p>
+          <div>{this.page}</div>
+        </p>
+        break;
+      default:
+        a=<p>
+          <div>FUCK</div>
+        </p>
+        break;
+    }
+    return (a)
+  }
 }
 
 export default App;
